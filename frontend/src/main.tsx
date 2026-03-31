c348214cbb4040f2bf8c66737c87c1327f428ab6
Joshua Katumba <joshuakatumba@gmail.com>
2026-03-31 19:02:00 +0300

    ast message

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app";
 import "./index.css";
-import { images } from "./assets/images.js";
+import { images } from "./assets/images";
 
 createRoot(document.getElementById("root")!).render(<App />);
